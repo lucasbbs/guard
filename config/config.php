701d61249085be1503f160ab50e52405b516181a
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2026-03-01 20:57:08 -0500

    remove hardcoded variables

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'database' => [
+
+        'driver' => 'sqlite',
+        'database' => base_path('database/database.sqlite')
+    ],
+    'security' => [
+        'first_key' => env('ENCRYPT_FIRST_KEY', base64_encode('')),
+        'second_key' => env('ENCRYPT_SECOND_KEY', base64_decode('')),
+    ]
+
+];
