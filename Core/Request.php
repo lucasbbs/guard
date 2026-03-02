ef854ee8db5fa4eb52074bcc96c203643109f293
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2026-03-01 20:49:22 -0500

    feat(core): add uploads + image validation

---
+++ Core/Request.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Core;
+
+class Request
+{
+  public function get($key, $default = null, $prefix = null)
+  {
+    return isset($_GET[$key])
+      ? ($prefix ?: null) . $_GET[$key]
+      : $default;
+  }
+
+  public function post($key = null, $default = null, $prefix = null)
+  {
+    if ($key === null) {
+      return $_POST;
+    }
+    return isset($_POST[$key])
+      ? ($prefix ?: null) . $_POST[$key]
+      : $default;
+  }
+
+  public function file($key = null, $default = null)
+  {
+    if ($key === null) {
+      return $_FILES;
+    }
+    if (! isset($_FILES[$key]) || ! is_array($_FILES[$key])) {
+      return $default;
+    }
+
+    $file = $_FILES[$key];
+    if (($file['error'] ?? UPLOAD_ERR_NO_FILE) === UPLOAD_ERR_NO_FILE) {
+      return $default;
+    }
+
+    return $file;
+  }
+}
